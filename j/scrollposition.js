7c9a2365a7255d3bfc4246ae76ab8870b1fc0ca5
Jeremy Boggs <jeremy@clioweb.org>
2014-03-27 15:38:23 -0400

    Readds background image parallax.

--- j/scrollposition.js
+++ j/scrollposition.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
 
     var $w = $(window);
+    var $scrollTop = $w.scrollTop();
+
     var $banner = $('#banner');
     var $bannerHeight = $banner.height();
+    var $articleHeader = $('article > header');
 
-    $w.scroll( function()
-    {
-		var $scrollTop = $w.scrollTop();
-		var $transformOffset = $scrollTop/2;
-        var $opacityOffset = ($bannerHeight - $transformOffset) / $bannerHeight;
+    $w.scroll( function() {
+
+		$scrollTop = $w.scrollTop();
+
+		var $yOffset = $scrollTop/2;
+        var $opacityOffset = ($bannerHeight - $yOffset) / $bannerHeight;
+        var $position = '50% -' + $yOffset + 'px';
 
 		$banner.css(
 		    {
-		        transform: "translateY(" + $transformOffset + "px)",
+		        transform: "translateY(" + $yOffset + "px)",
 		        opacity: $opacityOffset
 		    }
 		);
+
+		$articleHeader.css(
+            { 
+                backgroundPosition: $position
+            }
+        );
 
 	});
 
